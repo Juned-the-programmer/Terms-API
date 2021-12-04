aac890c0571b13e9d5caa7cb5ea9d62414c4f112
Juned-the-programmer <davdajuned7698@gmail.com>
2021-12-04 18:23:14 +0530

    static

--- Terms/settings.py
+++ Terms/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+BASE_DIR_HOST = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -119,9 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS=(
-    BASE_DIR / 'static'
+     os.path.join(BASE_DIR_HOST , 'static'),
 )
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -136,5 +141,5 @@ MEDIA_URL = '/media/'
 # STATIC_ROOT = os.path.join(BASE_DIR_HOST , "live-static-files","static-root")
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_ROOT = BASE_DIR / 'media'
-STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_ROOT = os.path.join(BASE_DIR_HOST , "live-static-files", "media-root")
+STATIC_ROOT = os.path.join(BASE_DIR_HOST , "live-static-files","static-root")
